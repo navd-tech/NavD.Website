ad14e1cfdec914c7a5343ddb762a771767a72942
Christos Charmatzis <xr_xarmatzis@outlook.com>
2019-06-16 20:52:06 +0300

    add enviromental variables

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    DEBUG: process.env.NODE_ENV === 'development',
-  
     siteName: 'NavD | The AI maritime Platform',
     author: 'christos.charmatzis',
     description: 'NavD is a cutting edge platform for AI solutions and large scale spatial analytics in the marine domain.',
@@ -10,11 +8,9 @@ module.exports = {
     twitter: '@NavD_Tech',
     type: 'website',
     googleAnalytics: 'UA-125551144-1',
-  
     backgroundColor: '#23232',
     themeColor: '#522bac',
     useCoolies: false,
     redirectionTimeout: 5000,
-    contactapi: //" http://localhost:7071/api"
-    "https://contactapifunctions20190612035239.azurewebsites.net/api"
+    contactapi: `${process.env.GATSBY_contactapi}`
   };
